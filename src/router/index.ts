906f26f62c4265b6512a5da6719d949936d4f1a7
mgzu <2931170625@qq.com>
2025-12-17 19:27:33 +0800

    refactor: router use hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import type { Router } from 'vue-router';
 import routes from './routes';
 
 const router: Router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: routes,
 });
 
