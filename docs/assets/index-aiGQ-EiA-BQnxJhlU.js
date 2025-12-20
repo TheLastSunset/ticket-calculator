3284be8af25a16f87461731e888fcac06b2a9be7
mgzu <2931170625@qq.com>
2025-12-20 23:28:20 +0800

    refactor: publish

---
+++ docs/assets/index-aiGQ-EiA-BQnxJhlU.js
@@ -0,0 +1,5 @@
+import { G as r } from './index-DTlM0rv7.js';
+function o() {
+  return r().proxy.$router || null;
+}
+export { o as u };
---
+++ docs/assets/renderIcon-CfE94nuJ-BSo14uZo.js
@@ -0,0 +1,3 @@
+import { Y as o } from './index-DTlM0rv7.js';
+const r = (r, s) => (r ? o(r, s) : '');
+export { r };
